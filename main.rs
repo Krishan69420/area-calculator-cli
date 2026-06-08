9057ffd66b780b162f4ce8c838cc9907d7d163ad
Krishan69420 <krishanmadhavsingh@gmail.com>
2026-06-08 14:32:14 +0530

    Refactor area calculation to use Shape enum

--- main.rs
+++ main.rs
@@ -1,5 +1,12 @@
 use std::io;
 
+enum Shape{
+    Rectangle,
+    Square,
+    Circle,
+    Triangle,
+    Trapezium,
+}
 fn main(){
     println!("Area Calculator");
     println!("Choose a shape:");
@@ -10,38 +17,46 @@ fn main(){
     println!("5. Trapezium");
 
     let choice = read_number("enter your choice 1-5") as u32;
-     
-    let area = match choice{
-        1 =>{
+
+    let shape = match choice {
+        1 => Shape::Rectangle,
+        2 => Shape::Square,
+        3 => Shape::Circle,
+        4 => Shape::Triangle,
+        5 => Shape::Trapezium,
+        _ => {
+            println!("Invalid choice.");
+            return;
+        }
+    };
+    let area = match shape {
+        Shape::Rectangle =>{
         let length:f64 = read_number("enter the length of rectangle");
         let width:f64 = read_number("enter the width of the rectangle");
         rectangle_area(length,width)
         }
-        2 =>{
+        Shape::Square =>{
         let side:f64 = read_number("enter the side of the square");    
         square_area(side)     
         }
-        3 =>{
+        Shape::Circle =>{
         let rad:f64 = read_number("enter the radius of the circle:");
         circle_area(rad)
         }
-        4 =>{
+        Shape::Triangle =>{
         let base:f64 = read_number("enter the base of the triangle");
         let height:f64 = read_number("enter the height of the triangle");
         triangle_area(base, height)
         }
-        5 =>{
+        Shape::Trapezium =>{
         let a = read_number("Enter first parallel side: ");
         let b = read_number("Enter second parallel side: ");
         let height = read_number("Enter height: ");
         trapezium_area(a, b, height)
         }
-        _ =>{
-            println!("enter valid input");
-            return;
-         }
+        
     };
- println!("the are is {}",area);
+ println!("the area is {}",area);
 }
 
 fn read_number(prompt: &str) -> f64 {
@@ -59,13 +74,13 @@ fn read_number(prompt: &str) -> f64 {
 }
 
 fn rectangle_area(length:f64,width:f64) ->f64{
-    lenght * width
+    length * width
 }
 fn square_area(side:f64) -> f64{
     side * side
 }
 fn circle_area(rad:f64) -> f64{
-    std::consts::PI*rad*rad
+    std::f64::consts::PI*rad*rad
 }
 fn triangle_area(base:f64, height:f64) -> f64{
     0.5*base*height
